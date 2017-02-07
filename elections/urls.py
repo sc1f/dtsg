12792922aa137b62f33583231003ea8bc028a84a
sc1f <tanjunyuan@hotmail.co.uk>
2017-02-07 10:59:36 -0600

    basic routing/templating frameworks, established basic models

--- elections/urls.py
+++ elections/urls.py
@@ -3,5 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # index page for elections
     url(r'^$', views.index, name='index'),
+    # get election by year
+    url(r'^(?P<year>[0-9]{4})/$', views.election, name='election'),
+    # get race by election year
+    url(r'^(?P<year>[0-9]{4})/race/(?P<race_id>[0-9])/$', views.race, name='race'),
+    # get candidate by year and id
+    url(r'^(?P<year>[0-9]{4})/(?P<race_id>[0-9])/candidate/(?P<candidate_id>[0-9])/$', views.candidate, name='candidate')
 ]
--- elections/views.py
+++ elections/views.py
@@ -1,5 +1,29 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Election, Race, Candidate
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Elections index.")
+    elections = Election.objects.all()
+    return render(request, 'elections/index.html', {'Elections': elections})
+
+def election(request, year):
+    selected_election = Election.objects.get(year=year)
+    races = Race.objects.filter(year_id=selected_election.id).all()
+    return render(request, 'elections/election.html', {'election': selected_election, 'races': races})
+
+def race(request, year, race_id):
+    selected_race = Race.objects.get(id=race_id)
+
+    candidates = Candidate.objects.filter(race_id=race_id)
+
+    return render(request, 'elections/race.html', {
+        'race': selected_race,
+        'Candidates': candidates,
+        'year': year
+    })
+
+def candidate(request, year, race_id, candidate_id):
+    selected_race = Race.objects.get(id=race_id)
+    selected_candidate = Candidate.objects.get(id=candidate_id)
+
+    return render(request, 'elections/candidate.html', {'race': selected_race, 'candidate': selected_candidate})
