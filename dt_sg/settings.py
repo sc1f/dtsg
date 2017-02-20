b77e6d009c199d08f35382392c44e2a14ed9c942
sc1f <tanjunyuan@hotmail.co.uk>
2017-02-19 23:04:47 -0600

    new requirements

--- dt_sg/settings.py
+++ dt_sg/settings.py
@@ -23,11 +23,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # TODO figure out settings for remote prod
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
