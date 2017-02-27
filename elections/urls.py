e5b4c8ef3e0ba685cb2e6e0408802650bfbc55f9
sc1f <tanjunyuan@hotmail.co.uk>
2017-02-27 17:44:15 -0600

    fix h3 and url overflow err

--- elections/urls.py
+++ elections/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     # get election by year
     url(r'^(?P<year>[0-9]{4})/$', views.races, name='races'),
     # get race by election year
-    url(r'^(?P<year>[0-9]{4})/race/(?P<race_id>[0-9])/$', views.positions, name='positions'),
+    url(r'^(?P<year>[0-9]{4})/race/(?P<race_id>[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])/$', views.positions, name='positions'),
     # get position and related candidates by year and id
-    url(r'^(?P<year>[0-9]{4})/(?P<race_id>[0-9])&(?P<position_id>[0-9])/$', views.candidates, name='candidates'),
+    url(r'^(?P<year>[0-9]{4})/(?P<race_id>[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])&(?P<position_id>[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])/$', views.candidates, name='candidates'),
     # candidate surveys
     url(r'^(?P<year>[0-9]{4})/add_candidate/$', views.add_candidate, name='add_candidate'),
 ]
