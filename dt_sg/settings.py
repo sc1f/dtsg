a4d65c737d40029a19d784f29f5b074e48c41503
Jun Tan <tanjunyuan@hotmail.co.uk>
2018-04-23 14:03:34 +0800

    revert

--- dt_sg/settings.py
+++ dt_sg/settings.py
@@ -127,10 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-"""
-handled by whitenoise
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-"""
 STATIC_URL = '/static/'
 
 
